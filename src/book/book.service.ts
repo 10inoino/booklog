564781d1686c30a4357b90b39cef5c93c18b4620
Hikaru Inomata <inomatahi@icloud.com>
2022-03-26 03:21:55 +0000

    サービスの関数作成

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { Book } from 'src/entities/book.entity';
+import { DeleteResult, InsertResult, Repository, UpdateResult } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateBookDTO } from './book.dto';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  constructor(
+    @InjectRepository(Book)
+    private readonly bookRepository: Repository<Book>,
+  ) {}
+
+  async findAll(): Promise<Book[]> {
+    return await this.bookRepository.find();
+  }
+
+  async create(book: CreateBookDTO): Promise<InsertResult> {
+    return await this.bookRepository.insert(book);
+  }
+
+  async find(id: number): Promise<Book> | null {
+    return await this.bookRepository.findOne({ id: id });
+  }
+
+  async update(id: number, book: CreateBookDTO): Promise<UpdateResult> {
+    return await this.bookRepository.update(id, book);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    return await this.bookRepository.delete(id);
+  }
+}
